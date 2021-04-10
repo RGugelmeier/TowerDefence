13a0289a51dd38ad9443387d483817c53e744080
RyanG <rgugelmeier@live.ca>
2021-04-10 00:05:32 -0400

    Initial Commit

---
+++ Assets/Scripts/Units/UnitBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBase : MonoBehaviour
+{
+    float moveSpeed;
+    float attackSpeed;
+    float attackRange;
+    float cost;
+    //ProjectileBase projectile; The projectile that the unit will fire. Projectile class needs to be made TODO
+    Rigidbody2D rb;
+    new BoxCollider2D collider;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+
+    }
+}
